660caf187673efa5d0f7d595b0b10f102153e4ad
Daniele Verducci (ZenPenguin) <danieleverducci@gmail.com>
2021-06-02 08:39:09 +0200

    Completed date checker script

--- jabs-date-checker.py
+++ jabs-date-checker.py
@@ -3,10 +3,22 @@
 """ @package docstring
 JABS - Just Another Backup Script - Backup recentness checking tool
 
-Monitors a folder for JABS backups and notifies an email address when
+Monitors a folder for JABS backups or snapshots and notifies an email address when
 the backup is outdated.
 
+Installation:
+- Copy jabs-date-checker.cfg in /etc/jabs/jabs-date-checker.cfg and customize it
+
+Usage:
+Place a cron entry like this one:
+
+0 5 * * *     root    /usr/local/bin/jabs-date-checker.py /etc/jabs/jabs-date-checker.cfg -q
+
+The script will end silently when has nothing to do.
+When the monitored backup is outside of its grace period, an email is sent to alert sysadmin.
+
 @author Daniele Verducci <daniele.verducci@gmail.com>
+
 This program is free software: you can redistribute it and/or modify
 it under the terms of the GNU General Public License as published by
 the Free Software Foundation, either version 3 of the License, or
@@ -25,10 +37,21 @@ import logging
 import traceback
 import subprocess
 import configparser
+from datetime import datetime, timedelta
+from email.mime.multipart import MIMEMultipart
+from email.mime.application import MIMEApplication
+from email.mime.text import MIMEText
+import smtplib
+import socket
+import getpass
+
 
 NAME = 'jabs-date-checker'
 VERSION = '0.1-alpha'
 DESCRIPTION = 'Backup recentness checking tool for JABS'
+EMAIL_SUBJECT_TPL = 'Backup of {} is TOO OLD!'
+EMAIL_MESSAGE_TPL = 'Last successfull run for backup set {} is {}: the backup may be outdated.'
+TIMESTAMP_FILENAME = 'backup-timestamp'
 
 class Main:
 	''' The recentness checker program '''
@@ -53,18 +76,100 @@ class Main:
 			if section == 'DEFAULT':
 				continue
 
+			s = Settings(section, self.config)
+			if s.disabled:
+				self._log.info('Ignoring disabled set "{}"'.format(section))
+				continue
+
 			self._log.info('Checking date for set "{}"'.format(section))
-			path = self.config.get(section, 'path')
 
 			# Read date file
-			f = open(path, "r")
-			print(f.read())
+			f = open(os.path.join(s.path, TIMESTAMP_FILENAME), "r")
+			backupDateStr = f.read().strip()
+			backupDate = datetime.strptime(backupDateStr, '%Y-%m-%d %H:%M:%S.%f')
+			gpDate = datetime.now() - timedelta(days=int(s.gracePeriod))
+
+			logging.info('Last backup date is {}, grace period is {}'.format(backupDate, gpDate))
 
 			# Check date isn't older than user-defined value
+			if backupDate < gpDate:
+				# Backup is older
+				logging.info('Backup is too old: sending mail')
+				self.sendMail(s, backupDate)
+			else:
+				logging.info('Backup is still valid. Nothing to do')
 
-			# Send email
+	def sendMail(self, s, backupDate):
+		if s.smtphost:
+			logging.info("Sending detailed logs to %s via  %s", s.mailto, s.smtphost)
+		else:
+			logging.info("Sending detailed logs to %s using local smtp", s.mailto)
 
+		# Create main message
+		msg = MIMEMultipart()
+		msg['Subject'] = EMAIL_SUBJECT_TPL.format(s.name)
+		if s.mailfrom:
+			m_from = s.mailfrom
+		else:
+			m_from = s.username + "@" + s.hostname
+		msg['From'] = m_from
+		msg['To'] = ', '.join(s.mailto)
+		msg.preamble = 'This is a multi-part message in MIME format.'
 
+		# Add base text
+		body = EMAIL_MESSAGE_TPL.format(s.name, backupDate)
+		txt = MIMEText(body)
+		msg.attach(txt)
+
+		# Send the message
+		if s.smtpssl and s.smtphost:
+			smtp = smtplib.SMTP_SSL(s.smtphost, timeout=300)
+		else:
+			smtp = smtplib.SMTP(timeout=300)
+
+		if s.smtphost:
+			smtp.connect(s.smtphost)
+		else:
+			smtp.connect()
+		if s.smtpuser or s.smtppass:
+			smtp.login(s.smtpuser, s.smtppass)
+		smtp.sendmail(m_from, s.mailto, msg.as_string())
+		smtp.quit()
+
+class Settings:
+	''' Represents settings for a backup set '''
+
+	EMAIL_LIST_SEP = ','
+
+	def __init__(self, name, config):
+		self.config = config
+		self.hostname = socket.getfqdn()
+		self.username = getpass.getuser()
+
+		## Backup set name
+		self.name = name
+		## Disabled
+		self.disabled = config.getboolean(name, 'DISABLED')
+		## Last backup path (mandatory)
+		self.path = config.get(name, 'path')
+		## Grace period in which the backup is considered still valid
+		self.gracePeriod = self.getStr(name, 'notify_older_days', 1)
+		## Email server connection data
+		self.smtphost = self.getStr(name, 'SMTPHOST', None)
+		self.smtpuser = self.getStr(name, 'SMTPUSER', None)
+		self.smtppass = self.getStr(name, 'SMTPPASS', None)
+		self.smtpssl = config.getboolean(name, 'SMTPSSL')
+		## List of email address to notify about backup status (mandatory)
+		mailtoList = config.get(name, 'MAILTO')
+		self.mailto = [ x.strip() for x in mailtoList.strip().split(self.EMAIL_LIST_SEP) ]
+		## Sender address for the notification email
+		self.mailfrom = self.getStr(name, 'MAILFROM', getpass.getuser()+'@'+socket.gethostname())
+
+	def getStr(self, name, key, defaultValue):
+		try:
+			return self.config.get(name, key)
+		except configparser.NoOptionError:
+			return defaultValue
 
 if __name__ == '__main__':
 	import argparse
